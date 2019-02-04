cb1d30272db224e5b7f828dae3c8852e5557ded3
torben <t.sickert[~at~]gmail.com>
2019-02-04 10:47:47 +0100

    Automatic update to new version.

--- filelinker.py
+++ filelinker.py
@@ -4,6 +4,17 @@
 # region header
 
 '''
+    [Project page](https://torben.website/dockerbase)
+
+    Copyright Torben Sickert (info["~at~"]torben.website) 16.12.2012
+
+    License
+    -------
+
+    This library written by Torben Sickert stand under a creative commons \
+    naming 3.0 unported license. \
+    See https://creativecommons.org/licenses/by/3.0/deed.de
+
     This module monitors a given file path. And relinks a broken hard link if \
     necessary.
 '''
